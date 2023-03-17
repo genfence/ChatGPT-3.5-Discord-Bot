9b2dcdda99f8ddf290da24da25972db43984c25f
Raymond <s95081@gmail.com>
2023-03-16 23:29:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('The Discord Bot online Now');
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.CHATGPT_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.DISCORD_CHANNEL_ID) return;
+  if (message.content.startsWith('!')) return;
+
+  let conversationLog = [{ role: 'system', content: 'You are GPT-3.5 Turbo Bot' }];
+
+  try {
+    await message.channel.sendTyping();
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+      if (message.content.startsWith('!')) return;
+      if (msg.author.id !== client.user.id && message.author.bot) return;
+      if (msg.author.id !== message.author.id) return;
+
+      conversationLog.push({
+        role: 'user',
+        content: msg.content,
+      });
+    });
+
+    const result = await openai
+      .createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+        max_tokens: 100
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+
+    message.reply(result.data.choices[0].message);
+  } catch (error) {
+    console.log(`ERR: ${error}`);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
